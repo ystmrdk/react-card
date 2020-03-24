d4b1f03d62f8f3938994705198386c9e3952d848
ystmrdk <rattyasta1@gmail.com>
2020-03-25 00:53:22 +0700

    statefull-component with api

--- src/modules/statefull-component.jsx
+++ src/modules/statefull-component.jsx
@@ -1,43 +1,57 @@
 import React from "react";
 
-import { Card, Avatar } from "antd";
-
-const { Meta } = Card;
-
 class Test extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: "1",
-      name: "Yasta",
-      job: "Nganggur"
+      data: [],
+      temp: null,
+      city: null,
+      temp: null,
+      weather: null,
     };
   }
 
+  async componentDidMount() {
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?q=Sleman&appid=69db624d6768601eeb046d6a847ee587&lang=id&units=metric";
+    const response = await fetch(url)
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            city: result.name,
+            weather: result.weather[0].description,
+            temp: result.main.temp,
+          });
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
   render() {
-    return (
-      <div>
-        <h3> This is stateful component</h3>
-
-        <Card
-          style={{ width: 200 , marginLeft: 50}}
-          cover={
-            <img
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-            />
-          }
-        >
-          <Meta
-            avatar={
-              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            }
-            title={this.state.name}
-            description={this.state.job}
-          />
-        </Card>
-      </div>
-    );
+    const { error, isLoaded, city ,temp ,weather } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <h1>{city}</h1>
+      <h2>{temp} {weather}</h2>
+        </div>
+      );
+    }
   }
 }
 
